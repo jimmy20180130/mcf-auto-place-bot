368bae0273d95b3dd0c0e86ba84e823df4c6ba3f
XiaoXi_TW <68285469+jimmy20180130@users.noreply.github.com>
2024-05-05 09:28:22 +0800

    Fixed Bugs

--- setup.bat
+++ setup.bat
@@ -1,7 +1,8 @@
 @echo off
 cls
-echo bot will restart when it crashes
+echo starting to setup the bot
 title Jimmy Bot
+echo make sure you have installed Node.js
 
 :: 檢查是否以系統管理員執行
 net session >nul 2>&1
@@ -25,7 +26,7 @@ if %errorLevel% neq 0 (
 :: 如果 update.txt 不存在，就下載整個檔案
 if not exist "%~dp0\update.txt" (
     echo file does not exist
-    powershell -Command Invoke-WebRequest -Uri "https://github.com/jimmy20180130/mcf-auto-place-bot/archive/refs/heads/main.zip" -OutFile "$env:TEMP\bot.zip"
+    powershell -Command Invoke-WebRequest -Uri "https://github.com/jimmy20180130/mcf-bet-public/archive/refs/heads/main.zip" -OutFile "$env:TEMP\bot.zip"
     move "%TEMP%\bot.zip" "%~dp0\bot.zip"
 ) else (
     echo file exists
@@ -36,18 +37,6 @@ if exist "%~dp0\bot.zip" (
     echo Decompressing zip file...
     powershell -Command "Expand-Archive -Path '%~dp0\bot.zip' -DestinationPath '%~dp0'"
     del "%~dp0\bot.zip"
-    echo file decompressed
 ) else (
     echo file decompressed
 )
-
-:: 安裝套件
-echo Installing packages...
-call npm i minecraft-data minecraft-protocol mineflayer prismarine-entity readline readline-sync silly-datetime moment-timezone
-echo packages installed
-
-:: 啟動 bot
-start node %~dp0/mcf-auto-place-bot-main/start.js
-
-:: exit
-exit /b
--- start.bat
+++ start.bat
@@ -3,8 +3,17 @@ cls
 echo bot will restart when it crashes
 title Jimmy Bot
 
+echo checking environment
+:: 安裝套件
+call npm install .
+
+title Jimmy Bot
+
 :StartServer
+echo (%time%) updating the bot
+start /min node update.js
+timeout /t 5
 echo (%time%) starting the bot
-start /wait node %~dp0/mcf-auto-place-bot-main/start.js
+start /wait node start.js
 echo (%time%) restarting the bot
 goto StartServer
